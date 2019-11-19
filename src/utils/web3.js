9147d2f0c5e2620078fa5a273a0c45262e14787d
Restyled.io <commits@restyled.io>
2019-11-19 01:04:43 +0000

    Restyled by clang-format

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,15 +1,15 @@
 // Libraries
+import Transport from "@ledgerhq/hw-transport-u2f";
 import Web3 from "web3";
 import * as Web3ProviderEngine from "web3-provider-engine/dist/es5";
 import * as RpcSource from "web3-provider-engine/dist/es5/subproviders/rpc";
-import Transport from "@ledgerhq/hw-transport-u2f";
+
+// Settings
+import * as settings from "../settings";
 
 // Utils
 import LedgerSubProvider from "./ledger-subprovider";
 import TrezorSubProvider from "./trezor-subprovider";
-
-// Settings
-import * as settings from "../settings";
 
 export const getWebClientProviderName = () => {
   if (window.imToken)
@@ -42,69 +42,81 @@ export const getWebClientProviderName = () => {
   if (window.web3.currentProvider.constructor.name === "Web3FrameProvider")
     return "parity";
 
-  if (window.web3.currentProvider.host && window.web3.currentProvider.host.indexOf("infura") !== -1)
+  if (window.web3.currentProvider.host &&
+      window.web3.currentProvider.host.indexOf("infura") !== -1)
     return "infura";
 
-  if (window.web3.currentProvider.host && window.web3.currentProvider.host.indexOf("localhost") !== -1)
+  if (window.web3.currentProvider.host &&
+      window.web3.currentProvider.host.indexOf("localhost") !== -1)
     return "localhost";
 
   return "other";
 };
 
 class Web3Extended extends Web3 {
-  stop = () => {
-    this.reset();
-    if (this.currentProvider && typeof this.currentProvider.stop === "function") {
-      this.currentProvider.stop();
-    }
-  }
-
-  setHWProvider = (device, network, path, accountsOffset = 0, accountsLength = 1) => {
-    this.stop();
-    return new Promise(async (resolve, reject) => {
-      try {
-        const networkId = network === "main" ? 1 : (network === "kovan" ? 42 : "");
-        this.setProvider(new Web3ProviderEngine());
-        const hwWalletSubProvider = device === "ledger"
-                                    ? LedgerSubProvider(async () => await Transport.create(), {networkId, path, accountsOffset, accountsLength})
-                                    : TrezorSubProvider({networkId, path, accountsOffset, accountsLength});
-        this.currentProvider.name = device;
-        this.currentProvider.addProvider(hwWalletSubProvider);
-        this.currentProvider.addProvider(new RpcSource({rpcUrl: settings.chain[network].nodeURL}));
-        this.currentProvider.start();
-        this.useLogs = false;
-        resolve(true);
-      } catch(e) {
-        reject(e);
-      }
-    });
-  }
-
-  setWebClientWeb3 = () => {
-    this.stop();
-    return new Promise(async (resolve, reject) => {
-      try {
-        if (window.web3 || window.ethereum) {
-          try {
-            let provider;
-            if (window.ethereum) {
-              await window.ethereum.enable();
-              provider = window.ethereum;
-            } else {
-              provider = window.web3.currentProvider;
-            }
-            resolve(provider);
-          } catch (error) {
-            reject(new Error("用户拒绝了访问"));
-          }
-        } else {
-          reject(new Error("没有找到客户端"));
+  stop =
+      () => {
+        this.reset();
+        if (this.currentProvider &&
+            typeof this.currentProvider.stop === "function") {
+          this.currentProvider.stop();
         }
-      } catch(e) {
-        reject(e);
       }
-    });
-  }
+
+  setHWProvider =
+      (device, network, path, accountsOffset = 0, accountsLength = 1) => {
+        this.stop();
+        return new Promise(async (resolve, reject) => {
+          try {
+            const networkId =
+                network === "main" ? 1 : (network === "kovan" ? 42 : "");
+            this.setProvider(new Web3ProviderEngine());
+            const hwWalletSubProvider =
+                device === "ledger"
+                    ? LedgerSubProvider(
+                          async () => await Transport.create(),
+                          {networkId, path, accountsOffset, accountsLength})
+                    : TrezorSubProvider(
+                          {networkId, path, accountsOffset, accountsLength});
+            this.currentProvider.name = device;
+            this.currentProvider.addProvider(hwWalletSubProvider);
+            this.currentProvider.addProvider(
+                new RpcSource({rpcUrl : settings.chain[network].nodeURL}));
+            this.currentProvider.start();
+            this.useLogs = false;
+            resolve(true);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+
+  setWebClientWeb3 =
+      () => {
+        this.stop();
+        return new Promise(async (resolve, reject) => {
+          try {
+            if (window.web3 || window.ethereum) {
+              try {
+                let provider;
+                if (window.ethereum) {
+                  await window.ethereum.enable();
+                  provider = window.ethereum;
+                } else {
+                  provider = window.web3.currentProvider;
+                }
+                resolve(provider);
+              } catch (error) {
+                reject(new Error("用户拒绝了访问"));
+              }
+            } else {
+              reject(new Error("没有找到客户端"));
+            }
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
 
   setWebClientProvider = provider => {
     return new Promise(async (resolve, reject) => {
